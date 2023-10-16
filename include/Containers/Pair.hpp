58d381ee9529b5e67166245e177465c1f53c3065
chriselrod <elrodc@gmail.com>
2023-10-16 02:17:50 -0400

    namespace Pair

--- include/Containers/Pair.hpp
+++ include/Containers/Pair.hpp
@@ -2,9 +2,11 @@
 #ifndef Pair_hpp_INCLUDED
 #define Pair_hpp_INCLUDED
 
+namespace poly::containers {
 template <class F, class S> struct Pair {
   [[no_unique_address]] F first;
   [[no_unique_address]] S second;
 };
+} // namespace poly::containers
 
 #endif // Pair_hpp_INCLUDED
