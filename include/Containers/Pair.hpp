6c1ce20a6966ec8cbfab0bd3b88910cfa7096ea2
chriselrod <elrodc@gmail.com>
2023-10-16 02:37:47 -0400

    cvt method for Pair

--- include/Containers/Pair.hpp
+++ include/Containers/Pair.hpp
@@ -1,11 +1,19 @@
 #pragma once
 #ifndef Pair_hpp_INCLUDED
 #define Pair_hpp_INCLUDED
+#include <type_traits>
 
 namespace poly::containers {
+
+template <typename To, typename From>
+concept ConvertibleFrom = std::is_convertible_v<From, To>;
+    
 template <class F, class S> struct Pair {
   [[no_unique_address]] F first;
   [[no_unique_address]] S second;
+  template <ConvertibleFrom<F> A, ConvertibleFrom<S> B> constexpr operator Pair<A, B>() {
+    return {A{first}, B{second}};
+  }
 };
 } // namespace poly::containers
 
